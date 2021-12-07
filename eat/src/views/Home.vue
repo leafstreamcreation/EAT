aa00a7486eeb00fa0a25648c78bae2303e4ec9c0
Derek <shouldergiant@icloud.com>
2021-12-07 16:50:19 -0500

    Basic meal suggestions

--- eat/src/views/Home.vue
+++ eat/src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-    <h1>EAT</h1>
+    <p>Your Meal Suggestion:</p>
+    <h1>{{ currentSuggestion || "I'm outta ideas" }}</h1>
+    <button @click="suggest(slop)">SLOP</button>
+    <button @click="suggest(notSlop)">NOT SLOP</button>
+    <button @click="suggest(eatOut)">EAT OUT</button>
+    <button @click="suggest(carbs)">CARBS</button>
   </div>
 </template>
 
@@ -9,7 +14,44 @@
 
 export default {
   name: "Home",
-  components: {
+  components: {},
+  data() {
+    return {
+      currentSuggestion: null,
+      slop: new Shuffler([
+        "Golumpkis",
+        "Pasta & Beef Sauce",
+        "Beef Cauliflower Rice",
+      ]),
+      notSlop: new Shuffler(["Pork", "Steak"]),
+      eatOut: new Shuffler(["Plaza Azteca", "Chik Fil A", "Sophias"]),
+      carbs: new Shuffler(["Grilled Cheese & Soup", "Nachos"]),
+    };
+  },
+  methods: {
+    suggest(category) {
+      this.currentSuggestion = category.drawNext();
+    },
   },
 };
+
+class Shuffler {
+  constructor(array) {
+    this.elements = [...array];
+    let remainingElements = this.elements.length,
+      elementToSwap,
+      nextElementIndex;
+
+    while (remainingElements > 0) {
+      nextElementIndex = Math.floor(Math.random() * remainingElements--);
+      elementToSwap = this.elements[remainingElements];
+      this.elements[remainingElements] = this.elements[nextElementIndex];
+      this.elements[nextElementIndex] = elementToSwap;
+    }
+  }
+
+  drawNext() {
+    return this.elements.pop();
+  }
+}
 </script>
